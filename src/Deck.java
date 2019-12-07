08476e969bfc172d49d590227820c290e43e6d83
Home-danielcarrillocardus <danielcarrillocardus@gmail.com>
2019-12-07 18:07:46 +0100

    adding customizable deck option

--- src/Deck.java
+++ src/Deck.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 public class Deck {
     Card[] deck;
 
-    // const
+    // const 1
     Deck(int deckType) {
         int aux = 0;         // auxiliar
-        switch (deckType){
+        switch (deckType) {
             case 1:
                 deck = new Card[48];
                 // oros
@@ -52,7 +52,7 @@ public class Deck {
 
                 // bastos
                 for (int i = 0; i < 12; i++) {
-                    if (i != 7 && i != 8){
+                    if (i != 7 && i != 8) {
                         Card b = new Card(Card.Suite.BASTOS, i + 1);
                         deck[aux] = b;
                         aux++;
@@ -78,11 +78,120 @@ public class Deck {
                 }
 
                 break;
+        }
+    }
 
-            case 3:
-                deck = new Card[36];
-                //TODO
-                break;
+    // const 2
+    Deck(String numbersToDelete) {
+
+        // find how many numbers to delete
+        int numberOfNoNumbers = 0;
+        for (int i = 0; i < numbersToDelete.length(); i++) {
+            if (i == numbersToDelete.length() - 1) {
+                if (numbersToDelete.charAt(i) == ','){
+                    numberOfNoNumbers++;
+                }
+            } else {
+                if (numbersToDelete.charAt(i) == ','){
+                    numberOfNoNumbers++;
+                } else if (numbersToDelete.charAt(i + 1) >= 48 && numbersToDelete.charAt(i + 1) <= 57){
+                    numberOfNoNumbers++;
+                }
+            }
+        }
+
+        // creating array of numbers to delete
+        int[] numbers = new int[numbersToDelete.length() - numberOfNoNumbers];
+        System.out.println(numbersToDelete.length() - numberOfNoNumbers);
+
+        // find which numbers to delete
+        int aux = 0;
+        for (int i = 0; i < numbersToDelete.length(); i++) {
+            String number = "";
+            if (numbersToDelete.charAt(i) >= 48 && numbersToDelete.charAt(i) <= 57) {
+                if (i == numbersToDelete.length() - 1) {
+                    if (numbersToDelete.charAt(i - 1) >= 48 && numbersToDelete.charAt(i - 1) <= 57) {
+                        break;
+                    } else {
+                        numbers[aux] = Integer.parseInt(String.valueOf(numbersToDelete.charAt(i)));
+                    }
+                } else {
+                    if (numbersToDelete.charAt(i + 1) >= 48 && numbersToDelete.charAt(i + 1) <= 57) {
+                        numbers[aux] = Integer.parseInt(number.concat(String.valueOf(numbersToDelete.charAt(i)) + String.valueOf(numbersToDelete.charAt(i + 1))));
+                    } else {
+                        numbers[aux] = Integer.parseInt(String.valueOf(numbersToDelete.charAt(i)));
+                    }
+                }
+                aux++;
+            }
+        }
+
+        // creating the deck
+        deck = new Card[48 - (numbers.length * 4)];
+        aux = 0;
+        boolean validNumber = true;
+        // oros
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i + 1 == numbers[j]) {
+                    validNumber = false;
+                    break;
+                }
+            }
+            if (validNumber) {
+                Card o = new Card(Card.Suite.OROS, i + 1);
+                deck[aux] = o;
+                aux++;
+            }
+            validNumber = true;
+        }
+
+        // copas
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i + 1 == numbers[j]) {
+                    validNumber = false;
+                    break;
+                }
+            }
+            if (validNumber) {
+                Card c = new Card(Card.Suite.COPAS, i + 1);
+                deck[aux] = c;
+                aux++;
+            }
+            validNumber = true;
+        }
+
+        // bastos
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i + 1 == numbers[j]) {
+                    validNumber = false;
+                    break;
+                }
+            }
+            if (validNumber) {
+                Card b = new Card(Card.Suite.BASTOS, i + 1);
+                deck[aux] = b;
+                aux++;
+            }
+            validNumber = true;
+        }
+
+        // espadas
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i + 1 == numbers[j]) {
+                    validNumber = false;
+                    break;
+                }
+            }
+            if (validNumber) {
+                Card e = new Card(Card.Suite.ESPADAS, i + 1);
+                deck[aux] = e;
+                aux++;
+            }
+            validNumber = true;
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,44 @@ public class Main {
         // initializing scanner
         Scanner sc = new Scanner(System.in);
 
+        Deck deck;
+
         // printing menu
         System.out.printf("%n %s %n", "Deck type: ");
         System.out.println("--------------------------------------------------------------------------------------------");
         System.out.printf("%-6s %-40s %s %n", "[1]", "Standard", "(48 cards)");
         System.out.println("--------------------------------------------------------------------------------------------");
-        System.out.printf("%-6s %-40s %s %n", "[2]", "Standard deck, except [8,9]", "(40 cards)");
+        System.out.printf("%-6s %-40s %s %n", "[2]", "Standard deck, except {8,9}", "(40 cards)");
         System.out.println("--------------------------------------------------------------------------------------------");
         System.out.printf("%-6s %-40s %s %n", "[3]", "Customizable" ,"(Decide which cards won't be include)");
         System.out.println("--------------------------------------------------------------------------------------------");
         System.out.print("Type your answer: ");
+        int answer = sc.nextInt();
 
-        switch (sc.nextInt()) {
+        switch (answer) {
             case 1:
+                // standard
                 Deck d1 = new Deck(1);
+                System.out.println("Your deck is: ");
                 d1.printDeck();
                 break;
             case 2:
+                // standard - {8, 9}
                 Deck d2 = new Deck(2);
+                System.out.println("Your deck is: ");
                 d2.printDeck();
                 break;
+            case 3:
+                // customizable
+                System.out.print("Which are the numbers you don't want to appear in your deck?: ");
+                String numbersToDelete = sc.next();
+                Deck d3 = new Deck(numbersToDelete);
+                System.out.println("Your deck is: ");
+                d3.printDeck();
+                break;
         }
+
+        // why i can't work with d1/d2/d3...?¿
+
     }
 }
